4c59be46d2bdedc9e4d331582f4a22b9065df3fc
Bowen Yin <bowenvictoryin@gmail.com>
2021-01-31 22:03:55 +0000

    Link syringe data

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="white" elevate-on-scroll>
       <div class="d-flex align-center">
-        <h3>Vaccination Tracker</h3>
+        <h3>U.S. Vaccination Tracker</h3>
       </div>
 
       <v-spacer></v-spacer>
