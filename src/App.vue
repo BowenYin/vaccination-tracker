ee8cc5a201da47e0bd6e68a43028fcf494fc9fa1
Bowen Yin <bowenvictoryin@gmail.com>
2021-02-20 04:31:59 +0000

    Add About button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <v-app>
     <v-app-bar app color="white" elevate-on-scroll>
-      <div class="d-flex align-center">
+      <a href="/" class="d-flex align-center text-decoration-none" style="color: inherit;">
         <h3 v-if="$vuetify.breakpoint.smAndUp">U.S. Vaccination Tracker</h3>
         <h4 v-else>U.S. Vaccination Tracker</h4>
-      </div>
+      </a>
 
       <v-spacer></v-spacer>
       <!--<v-btn icon>
         <v-icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">brightness_4</v-icon>
       </v-btn>-->
+      <v-btn href="/about" text large :ripple="false">About</v-btn>
     </v-app-bar>
 
     <v-main>
